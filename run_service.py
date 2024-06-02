0bff2d655a0dff0b00f0d68a0495975ab5879926
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-06-02 13:51:23 +0900

    asdf

--- run_service.py
+++ run_service.py
@@ -6,7 +6,9 @@ from geopy import geocoders
 from naver_search import search_res
 import requests
 
-def serve_code(location, token, crd): return send(token, location, search_res(location, menu_choose(dust_weather(weather(crd)[0], weather(crd)[1]))), dust_weather(weather(crd)[0], weather(crd)[1]))
+def serve_code(location, token, crd): 
+    d = dust_weather(weather(crd)[0], weather(crd)[1])
+    return send(token, location, search_res(location, menu_choose(d)), d)
 
 def get_token(code, client_id, redirect_uri): return requests.post('https://kauth.kakao.com/oauth/token', data = {'grant_type': 'authorization_code', 'client_id': client_id, 'redirect_uri': redirect_uri, 'code': code}).json().get('access_token')
 
@@ -14,4 +16,5 @@ def get_address(lat_lng):
     
     for i in range(len(str(geocoders.Nominatim(user_agent = 'South Korea', timeout = None).reverse(lat_lng)).split(', '))):
         a = str(geocoders.Nominatim(user_agent = 'South Korea', timeout = None).reverse(lat_lng)).split(', ')[i]
-        if '동' in a: return a
+        if '동' in a: 
+            return a
