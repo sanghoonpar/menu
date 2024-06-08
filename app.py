03e99c99c7b8f16087714cf36c8348521f5cbdb0
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-06-08 14:24:36 +0900

    asdf

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 access_token, crd, address = None, None, None
+nick_names = []
 app = Flask(__name__, template_folder = 'templates')
 
 @app.route('/', methods = ['GET', 'POST'])
