6acac786ab4679ebbf6d72121daf678a77a57259
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-06-16 09:50:33 +0900

    asdf

--- app.py
+++ app.py
@@ -11,23 +11,23 @@ app.secret_key = os.environ.get('sec_key')
 @app.route('/')
 def inintial(): 
 
-    if session.get('user'):
-        return render_template('home.html', user = session.get('user'))
-    if request.cookies.get('user_id'):
-        if get_user_from_db(request.cookies.get('user_id')):
-            session['user'] = get_user_from_db(request.cookies.get('user_id'))
-            return render_template('home.html', user = get_user_from_db(request.cookies.get('user_id')))
+    #if session.get('user'): return render_template('home.html', user = session.get('user'))
+    
+    #if request.cookies.get('user_id'):
+     #   if get_user_from_db(request.cookies.get('user_id')):
+      #      session['user'] = get_user_from_db(request.cookies.get('user_id'))
+       #     return render_template('home.html', user = get_user_from_db(request.cookies.get('user_id')))
     return render_template('home.html', user = None)
 
 @app.route('/login')
 def login(): return redirect(os.environ.get('c_b_u2'))
 
-@app.route('/logout')
-def logout():
-    session.pop('user', None)
-    resp = make_response(redirect(url_for('home')))
-    resp.set_cookie('user_id', '', expires = 0)
-    return resp
+#@app.route('/logout')
+#def logout():
+    #session.pop('user', None)
+    #resp = make_response(redirect(url_for('home')))
+    #resp.set_cookie('user_id', '', expires = 0)
+    #return resp
 
 @app.route('/main')
 def login():
@@ -52,13 +52,13 @@ def kakaocallback():
     access_token = run_service.get_token(request.args.get('code'), os.environ.get('k_cli_id'), os.environ.get('k_red_uri'))
     
     user_info = requests.get('https://kapi.kakao.com/v2/user/me', headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/x-www-form-urlencoded',}).json()
-    user_infor = {'id': user_info['id'], 'nickname': user_info['properties']['nickname'],}
-    user_inform = save_user_to_db(user_infor)
+    #user_infor = {'id': user_info['id'], 'nickname': user_info['properties']['nickname'],}
+    #user_inform = save_user_to_db(user_infor)
 
-    session['user'] = user_infor
-    resp = make_response(redirect(url_for('home')))
-    resp.set_cookie('user_id', str(user_info['id']), max_age = 30 * 24 * 60 * 60)
-    return render_template('kakaocallback.html', nick_name = user_info['properties']['nickname'], resp = resp)
+    #session['user'] = user_infor
+    #resp = make_response(redirect(url_for('home')))
+    #resp.set_cookie('user_id', str(user_info['id']), max_age = 30 * 24 * 60 * 60)
+    return render_template('kakaocallback.html', nick_name = user_info['properties']['nickname'])#, resp = resp)
 
 @app.route('/k_logout')
 def logout():
