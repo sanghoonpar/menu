4a9d1f8dbdba09121d4221ca6cd11c40081286ed
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-06-16 15:02:33 +0900

    adsf

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import webbrowser, os, run_service, requests
-from flask import Flask, render_template, request#, session, url_for, redirect, flash, make_response
+from flask import Flask, render_template, request, session#, url_for, redirect, make_response
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -7,27 +7,24 @@ access_token, crd, address = None, None, None
 
 app = Flask(__name__, template_folder = 'templates')
 app.secret_key = os.environ.get('sec_key')
+user = {}
 
 @app.route('/')
 def inintial(): 
+    return render_template('home.html')
 
-    #if session.get('user'): return render_template('home.html', user = session.get('user'))
+@app.route('/make_cookie', methods=['POST'])
+def make_cookie(): 
+    username = request.form['username']
+    password = request.form['password']    
+    if username in user and user[username]['password'] == password:
+       session['username'] = username
+
+    if 'username' in session: 
+       username = session['username']
+       return render_template('home.html', user = f'로그인 완료 {username}님')
     
-    #if request.cookies.get('user_id'):
-     #   if get_user_from_db(request.cookies.get('user_id')):
-      #      session['user'] = get_user_from_db(request.cookies.get('user_id'))
-       #     return render_template('home.html', user = get_user_from_db(request.cookies.get('user_id')))
-    return render_template('home.html')#, user = None)
-
-@app.route('/login')
-def login(): return os.environ.get('c_b_u')#redirect(os.environ.get('c_b_u2'))
-
-#@app.route('/logout')
-#def logout():
-    #session.pop('user', None)
-    #resp = make_response(redirect(url_for('home')))
-    #resp.set_cookie('user_id', '', expires = 0)
-    #return resp
+    return render_template('home.html', user = '로그인 실패')
 
 @app.route('/main')
 def login():
@@ -39,7 +36,7 @@ def login():
             crd = request.args.get('lat') + ', ' + request.args.get('lon')
 
     elif request.method == 'POST': webbrowser.open(os.environ.get('c_b_u'))
-
+    
     if access_token != None: key = 0
     else: key = 1
 
@@ -52,13 +49,17 @@ def kakaocallback():
     access_token = run_service.get_token(request.args.get('code'), os.environ.get('k_cli_id'), os.environ.get('k_red_uri'))
     
     user_info = requests.get('https://kapi.kakao.com/v2/user/me', headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/x-www-form-urlencoded',}).json()
-    #user_infor = {'id': user_info['id'], 'nickname': user_info['properties']['nickname'],}
-    #user_inform = save_user_to_db(user_infor)
+    print(user_info)
 
-    #session['user'] = user_infor
-    #resp = make_response(redirect(url_for('home')))
-    #resp.set_cookie('user_id', str(user_info['id']), max_age = 30 * 24 * 60 * 60)
-    return render_template('kakaocallback.html', nick_name = user_info['properties']['nickname'])#, resp = resp)
+    session['username'] = username
+
+    if 'username' in session: 
+       username = session['username']
+       return render_template('home.html', user = f'로그인 완료 {username}님')
+    
+
+
+    return render_template('kakaocallback.html', nick_name = user_info['properties']['nickname'])
 
 @app.route('/k_logout')
 def logout():
@@ -66,6 +67,8 @@ def logout():
     
     access_token = None
     return login()
+   # session.pop('user', None)
+    #return redirect(url_for('index')), login()
 
 @app.route('/map')
 def map(): return render_template('map.html', java_key = os.environ.get('k_java_key'))
@@ -78,16 +81,9 @@ def service():
     
     global address, access_token, crd
     if address != None and access_token != None:    
-        if run_service.serve_code(address, access_token, crd) == 1: return render_template('success.html')
+        if run_service.serve_code(address, access_token, crd) == 2: return render_template('success.html')
         else: return render_template('fail.html')
     else: return render_template('try_again.html')
-
-def save_user_to_db(user):
-    user_inform = {}
-    user_inform[user['id']] = user['id'], user['nickname']
-    return user_inform
-
-def get_user_from_db(user_id, user_inform): return {'id': user_id, 'nickname': user_inform[user_id]['nickname']}
 
 if __name__ == '__main__':
     with open('cert.pem', 'w') as certfile: certfile.write(os.environ.get('cert_str'))
--- menu_choose.py
+++ menu_choose.py
@@ -7,7 +7,7 @@ def menu_choose(data):
     else: weather_state = 3
 
     if weather_state == 1: foods_list = random.sample('부대찌개, 칼국수, 수제비, 짬뽕, 김치부침개, 파전'.split(', '), 3)
-    elif weather_state == 2: foods_list = random.sample('도라지, 미나리, 마늘, 녹차'.split(', '), 3)
+    elif weather_state == 2: foods_list = random.sample('도라지, 미나리, 마늘, 녹차, 오이'.split(', '), 3)
     else: foods_list = random.sample('한식, 중식, 양식, 일식'.split(', '), 3)
 
     return foods_list
