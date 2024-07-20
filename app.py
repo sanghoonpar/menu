9557a7a62dcdc63739643c775c88fff86782a27a
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-07-20 14:39:47 +0900

    asdf

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, request, session, url_for, redirect, render_template
 from dotenv import load_dotenv
 load_dotenv()
 
-access_token, crd, address = None, None, None
+n = None
+h = '.html'
+access_token, crd, address = n, n, n
 
 app = Flask(__name__, template_folder = 'templates')
 app.secret_key = os.environ.get('sec_key')
@@ -30,19 +32,19 @@ def login():
     global crd, address
 
     if request.method == 'GET':
-        if request.args.get('lat') != None and request.args.get('lon') != None:
+        if request.args.get('lat') != n and request.args.get('lon') != n:
             address = run_service.get_address(request.args.get('lat') + ', ' + request.args.get('lon'))
             crd = request.args.get('lat') + ', ' + request.args.get('lon')
 
     elif request.method == 'POST': webbrowser.open(os.environ.get('c_b_u'))
     
-    if access_token != None: key = 0
+    if access_token != n: key = 0
     else: key = 1
 
-    if crd != None: key1 = 0
+    if crd != n: key1 = 0
     else: key1 = 1
 
-    return render_template('main.html', c_b_u = os.environ.get('c_b_u'), key = key, key1 = key1)
+    return render_template('main' + h, c_b_u = os.environ.get('c_b_u'), key = key, key1 = key1)
 
 @app.route('/kakaocallback')
 def kakaocallback():
@@ -58,31 +60,31 @@ def kakaocallback():
     #    username = session['username']
     #    return render_template('main.html', user = f'로그인 완료 {username}님')
 
-    return render_template('kakaocallback.html', nick_name = user_info['properties']['nickname'])
+    return render_template('kakaocallback' + h, nick_name = user_info['properties']['nickname'])
 
 @app.route('/k_logout')
 def logout():
     global access_token, crd, address
 
-    access_token, crd, address = None, None, None
-    return render_template('home.html')
+    access_token, crd, address = n, n, n
+    return render_template('home' + h)
    # session.pop('user', None)
     #return redirect(url_for('index')), login()
 
 @app.route('/map')
-def map(): return render_template('map.html', java_key = os.environ.get('k_java_key'))
+def map(): return render_template('map' + h, java_key = os.environ.get('k_java_key'))
 
 @app.route('/manual')
-def manual(): return render_template('manual.html')
+def manual(): return render_template('manual' + h)
 
 @app.route('/service')
 def service():
     
     global address, access_token, crd
-    if address != None and access_token != None:
-        if run_service.serve_code(address, access_token, crd) == 2: return render_template('success.html')
-        else: return render_template('fail.html')
-    else: return render_template('try_again.html')
+    if address != n and access_token != n:
+        if run_service.serve_code(address, access_token, crd) == 2: return render_template('success' + h)
+        else: return render_template('fail' + h)
+    else: return render_template('try_again' + h)
 
 if __name__ == '__main__':
     with open('cert.pem', 'w') as certfile: certfile.write(os.environ.get('cert_str'))
