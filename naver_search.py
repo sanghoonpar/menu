ff7cceee7ed1341bf01b93a2fe9321517fe29121
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-07-21 14:06:12 +0900

    adf

--- naver_search.py
+++ naver_search.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 res = []
-def search_res(loca, listf):
+def search_res(loc, listf):
     for food in listf:
-        req = requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ loca + ' ' + food + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
+        req = requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ loc + ' ' + food + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
 
         if not req: print('검색 실패', food)
 
