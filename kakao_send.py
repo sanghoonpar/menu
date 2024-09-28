d59fc12d880e4e9db03790b133707d4446d93783
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-09-28 13:50:59 +0900

    asdf

--- kakao_send.py
+++ kakao_send.py
@@ -1,36 +1,36 @@
 import urllib, json, requests
 
-def send(tk, lc, res, dt):
+def send(a, b, c, d):
 
-    suc = 0
-    text = f'''\
-    위치 정보 : {lc}
-    날씨 정보 ({dt['date']})
-    기온 : {dt['weather']['tmp']}
-    기우  : {dt['weather']['state']}
-    미세먼지 : {dt['dust']['pm10']['value']} {dt['dust']['pm10']['state']}
+    e = 0
+    f = f'''\
+    위치 정보 : {b}
+    날씨 정보 ({d['date']})
+    기온 : {d['weather']['tmp']}
+    기우  : {d['weather']['state']}
+    미세먼지 : {d['dust']['pm10']['value']} {d['dust']['pm10']['state']}
     '''
 
-    def li(url): return {'web_url': url, 'mobile_web_url': url}
-    def req(): return requests.post(url = url, data = dat, headers = {'Authorization': 'Bearer ' + {'access_token' : tk}.get('access_token'), 'scope':'talk_message'}).json()
-    url = 'https://kapi.kakao.com/v2/api/talk/memo/default/send'
-    wea = li('https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=날씨')
-    dat = {'template_object' : json.dumps({'object_type': 'text', 'text': text, 'link': wea, 'button_title': '날씨 상세보기'})}
+    def n(g): return {'web_url': g, 'mobile_web_url': g}
+    def o(): return requests.post(url = g, data = i, headers = {'Authorization': 'Bearer ' + {'access_token' : a}.get('access_token'), 'scope':'talk_message'}).json()
+    g = 'https://kapi.kakao.com/v2/api/talk/memo/default/send'
+    h = n('https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=날씨')
+    i = {'template_object' : json.dumps({'object_type': 'text', 'text': f, 'link': h, 'button_title': '날씨 상세보기'})}
 
-    if req().get('result_code') == 0: suc += 1
-    else: print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ', str(req()))
+    if o().get('result_code') == 0: e += 1
+    else: print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ', str(o()))
 
-    ct = []
-    at = []
+    j = []
+    k = []
 
-    for pl in res:
-        tt = pl.get('title').replace('<b>','').replace('</b>','')
-        at.append(tt)
-        if pl.get('telephone'): tt = tt, '\ntel) ', pl.get('telephone')
-        ct.append({'title': '[' + pl.get('category') + '] ' + tt, 'description': ''.join(pl.get('address').split()[1:]), 'image_url': 'https://freesvg.org/img/bentolunch.png?w=150&h=150&fit=fill', 'image_width': 50, 'image_height': 50, 'link': li('https://search.naver.com/search.naver?query=' + urllib.parse.quote(pl.get('address') + '' + tt))})
+    for l in c:
+        m = l.get('title').replace('<b>','').replace('</b>','')
+        k.append(m)
+        if l.get('telephone'): m = m, '\ntel) ', l.get('telephone')
+        j.append({'title': '[' + l.get('category') + '] ' + m, 'description': ''.join(l.get('address').split()[1:]), 'image_url': 'https://freesvg.org/img/bentolunch.png?w=150&h=150&fit=fill', 'image_width': 50, 'image_height': 50, 'link': n('https://search.naver.com/search.naver?query=' + urllib.parse.quote(l.get('address') + '' + m))})
 
-    dat = {'template_object' : json.dumps({'object_type' : 'list', 'header_title' : '현재 날씨에 따른 음식 추천', 'header_link' : wea, 'contents' : ct, 'buttons' : [{'title' : '날씨 상세보기', 'link' : wea}]})}
-    if req().get('result_code') == 0: suc += 1
-    else: print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ', str(req()))
+    i = {'template_object' : json.dumps({'object_type' : 'list', 'header_title' : '현재 날씨에 따른 음식 추천', 'header_link' : h, 'contents' : j, 'buttons' : [{'title' : '날씨 상세보기', 'link' : h}]})}
+    if o().get('result_code') == 0: e += 1
+    else: print('메시지를 성공적으로 보내지 못했습니다. 오류메시지 : ', str(o()))
 
-    return [suc, at, dt['weather']['state'], dt['dust']['pm10']['value'], dt['weather']['tmp']]
+    return [e, k, d['weather']['state'], d['dust']['pm10']['value'], d['weather']['tmp']]
