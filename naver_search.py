6d3b674a3ed50cf986bd9c50207d021b13851f80
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-10-12 15:07:16 +0900

    asdf

--- naver_search.py
+++ naver_search.py
@@ -1,14 +1,18 @@
-import os, requests
+import os, requests, random
 from dotenv import load_dotenv 
 load_dotenv()
 
 r = []
 def s_r(l, f):
     q = requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ l + ' ' + f + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
-    if q != '[]':
+    if q != []:
         r.append(q[0])
         r.append(q[1])
-    else: requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ l + ' ' + '국밥' + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
+    else: 
+        f2 = random.sample('타코야끼, 찜닭, 카레, 샌드위치'.split(', '), 1)[0]
+        q = requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ l + ' ' + f2 + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
+        r.append(q[0])
+        r.append(q[1])
     for i in range(len(r) - 1):
         if r[0] == r[1]: r.remove(r[0])
     if len(r) >= 3:
