b294cfc8897a9d0e2daa8431b54433ea7e05e089
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-10-19 10:47:42 +0900

    asdf

--- menu_choose.py
+++ menu_choose.py
@@ -8,6 +8,6 @@ def m_c(d):
 
     if w == 1: f = random.sample('부대찌개, 칼국수, 수제비, 짬뽕, 김치부침개, 파전, 김치찌개'.split(', '), 3)
     elif w == 2: f = random.sample('도라지, 미나리, 마늘, 녹차, 오이'.split(', '), 3)
-    else: f = random.sample('김치찌개, 짜장면, 삼겹살, 치킨'.split(', '), 3)
+    else: f = random.sample('김치찌개, 짜장면, 삼겹살, 치킨, 국밥'.split(', '), 3)
 
     return f
