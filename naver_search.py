9e219183457bec52d7b653d24b0212e5267def63
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-10-19 15:11:30 +0900

    asdf

--- naver_search.py
+++ naver_search.py
@@ -4,8 +4,9 @@ load_dotenv()
 
 r = []
 def s_r(l, f):
-    while len(r) <= 2:
+    while len(r) <= 1:
         q = requests.get('https://openapi.naver.com/v1/search/local.json?', params = 'sort=comment&query='+ l + ' ' + f + ' 맛집&display=5', headers = {'X-Naver-Client-Id' : os.environ.get('n_cli_id'), 'X-Naver-Client-Secret' : os.environ.get('n_cli_secret')}).json().get('items')
+        print(q)
         if len(q) >= 2:
             r.append(q[0])
             r.append(q[1])
