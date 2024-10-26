33d0faf45b4c364a77ff00b4e6f0bfc075b68afc
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-10-26 13:59:28 +0900

    asd

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,5 +1,5 @@
 var CACHE_NAME = 'pwa-manager'; 
-var urlsToCache = ['/index.html',  '/common.js', '/style.css'];
+var urlsToCache = ['../templates/home.html', 'css/home.css'];
 
 // Install a service worker
 self.addEventListener('install', event => {
@@ -8,8 +8,7 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME)
             .then(function(cache) {
                 console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            }));});
+                return cache.addAll(urlsToCache)}))});
 // Cache and return requests
 self.addEventListener('fetch', event => {
     event.respondWith(
@@ -17,30 +16,20 @@ self.addEventListener('fetch', event => {
             .then(function(response) {
                 // Cache hit - return response
                 if (response) {return response;}
-                return fetch(event.request);
-            }));});
+                return fetch(event.request)}))});
 // Update a service worker
 self.addEventListener('activate', event => {
     var cacheWhitelist = ['pwa-manager'];
-    event.waitUntil(caches.keys().then(cacheNames => {return Promise.all(cacheNames.map(cacheName => {if (cacheWhitelist.indexOf(cacheName) === -1) {return caches.delete(cacheName);}}));}));
-});
+    event.waitUntil(caches.keys().then(cacheNames => {return Promise.all(cacheNames.map(cacheName => {if (cacheWhitelist.indexOf(cacheName) === -1) {return caches.delete(cacheName)}}))}))});
 let deferredPrompt = null;
 
 window.addEventListener('beforeinstallprompt', function(event) {
   event.preventDefault();
-  deferredPrompt = event;
-});
+  deferredPrompt = event});
 
 $("#a2hs_btn").on("click", function(e){
   e.preventDefault();
   if (deferredPrompt) {
     deferredPrompt.prompt();
-    deferredPrompt.userChoice.then(function (choiceResult) {
-      if (choiceResult.outcome === 'accepted') {
-        deferredPrompt = null;
-      }
-    });
-  } else {
-    alert("홈 화면에 추가할 수 없습니다.")
-  }
-});
+    deferredPrompt.userChoice.then(function (choiceResult) {if (choiceResult.outcome === 'accepted') {deferredPrompt = null}})} 
+  else {alert("홈 화면에 추가할 수 없습니다.")}});
