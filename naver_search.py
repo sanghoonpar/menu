16aeb10e27062d19377776fdcb0147f014b59318
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-11-02 14:10:07 +0900

    asdf

--- naver_search.py
+++ naver_search.py
@@ -2,8 +2,8 @@ import os, requests
 from dotenv import load_dotenv 
 load_dotenv()
 
-r = []
 def s_r(l, f):
+    r = []
     while len(r) <= 1:
         q = requests.get("https://openapi.naver.com/v1/search/local.json", headers = {"X-Naver-Client-Id": os.environ.get('n_cli_id'), "X-Naver-Client-Secret": os.environ.get('n_cli_secret')}, params = {"query": f"{l} {f} 맛집", "display": 10, "start": 1}).json().get('items')
         if len(q) >= 2:
@@ -11,5 +11,5 @@ def s_r(l, f):
             r.append(q[1])
     for i in range(len(r) - 1):
         if r[0] == r[1]: r.remove(r[0])
-
+    
     return r
