786ed8a39e542c08147c8ca2e9e6c24a3b993a0c
jsy1102jsy <156557625+jsy1102jsy@users.noreply.github.com>
2024-11-09 14:55:34 +0900

    asdf

--- app.py
+++ app.py
@@ -8,38 +8,31 @@ n = None
 h = ".html"
 def fetch_env(a): return os.environ.get(a)
 def fetch_url(b): return request.args.get(b)
-a_t, crd, ad, Id, dat, f_l, food, weat = n, n, n, n, [n,"?","?","?","?"], n, n, n
+access_token, coordinate, address, Id, result, food_list, food, data = n, n, n, n, [n,"?","?","?","?"], n, n, n
 
 app = Flask(__name__, template_folder = "templates")
 lucide = Lucide(app)
 
 @app.route("/")
 def inintial():
-    if a_t != n: key = 0
-    else: key = 1
-
-    if crd != n: key1 = 0
-    else: key1 = 1
     return render_template("home" + h, 
-                           c_b_u = fetch_env("c_b_u"), 
-                           key = key, 
-                           key1 = key1, 
-                           a = a_t)
+                           call_back_url = fetch_env("c_b_u"), 
+                           access_token = access_token)
 
 @app.route("/kakaocallback")
 def kakaocallback():
-    global a_t, Id
-    a_t = run_service.get_access_token(fetch_url("code"), fetch_env("k_cli_id"), fetch_env("k_red_uri"))
+    global access_token, Id
+    access_token = run_service.get_access_token(fetch_url("code"), fetch_env("k_cli_id"), fetch_env("k_red_uri"))
     Id = requests.get("https://kapi.kakao.com/v2/user/me", 
-                      headers = {"Authorization": f"Bearer {a_t}", 
+                      headers = {"Authorization": f"Bearer {access_token}", 
                                  "Content-Type": "application/x-www-form-urlencoded"}).json()["properties"]["nickname"]
     return render_template("kakaocallback" + h, 
                            nick_name = Id)
 
 @app.route("/logout")
 def logout():
-    global a_t, crd, ad, Id, dat, f_l, food, weat
-    a_t, crd, ad, Id, dat, f_l, food, weat = n, n, n, n, [n,"?","?","?","?"], n, n, n
+    global access_token, coordinate, address, Id, result, food_list, food, data
+    access_token, coordinate, address, Id, result, food_list, food, data = n, n, n, n, [n,"?","?","?","?"], n, n, n
     return render_template("lo" + h)
 
 @app.route("/map")
@@ -51,26 +44,26 @@ def manual(): return render_template("manual" + h)
 
 @app.route("/roulette")
 def gatcha(): 
-    global f_l, ad, weat
-    ad = run_service.get_address(fetch_url("lat") + ", " + fetch_url("lon"))
-    crd = fetch_url("lat") + ", " + (fetch_url("lon"))
-    weat = run_service.select_menu(crd, fetch_env("ser_key1"), fetch_env("ser_key2"))
-    f_l = menu_choose.menu_choose(weat)
+    global food_list, address, data
+    address = run_service.get_address(fetch_url("lat") + ", " + fetch_url("lon"))
+    coordinate = fetch_url("lat") + ", " + (fetch_url("lon"))
+    data = run_service.select_menu(coordinate, fetch_env("ser_key1"), fetch_env("ser_key2"))
+    food_list = menu_choose.menu_choose(data)
     return render_template("roulette" + h, 
-                           f_l = f_l)
+                           food_list = food_list)
 
 @app.route("/service", methods = ["Get", "Post"])
 def service():
-    global ad, a_t, crd, dat, food, weat
+    global address, access_token, coordinate, result, food, data
     food = fetch_url("food")
-    dat = run_service.run_service(ad, a_t, food, weat)
-    if ad != n and a_t != n:
-        if dat[0] == 2: return render_template("success" + h, 
-                                               rec = dat[1], 
-                                               ad = ad, 
-                                               wea = dat[2], 
-                                               dust = str(dat[3]) + "㎍/㎥", 
-                                               temp = str(dat[4]) + "°C", 
+    result = run_service.run_service(address, access_token, food, data)
+    if address != n and access_token != n:
+        if result[0] == 2: return render_template("success" + h, 
+                                               record = result[1], 
+                                               address = address, 
+                                               weather_data = result[2], 
+                                               dust = str(result[3]) + "㎍/㎥", 
+                                               temp = str(result[4]) + "°C", 
                                                id = Id)
         else: return render_template("fail" + h)
     else: return render_template("try_again" + h)
